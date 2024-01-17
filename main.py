801c4d91ff364e0efa82162305161f57e4662332
X <59506729+qweasdzxcrrffvv@users.noreply.github.com>
2024-01-17 20:37:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 # ==============================================================================
 query_dict = st.experimental_get_query_params()
-aid = query_dict['aid'][0]
-if aid == 'drug_dosage':
-    st.switch_page(f"pages/{aid}.py")
+if aid in query_dict.keys():
+    aid = query_dict['aid'][0]
+    if aid == 'drug_dosage':
+        st.switch_page(f"pages/{aid}.py")
